6ea5527a093fcec5a6634eba447a93edad6a3501
Omer AVCI <omer.avci@miniclip.com>
2021-06-13 14:54:51 +0100

    Outer Glow Shader started

---
+++ Shaders/Assets/Shaders/InProgress/OuterGlowShader/Demo/OuterGlowDemo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class OuterGlowDemo : MonoBehaviour
+{
+    [SerializeField]
+    private OuterGlowController outerGlowController;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+            outerGlowController.SetActive(!outerGlowController.enabled);
+    }
+}
